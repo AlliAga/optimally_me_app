bee51e6b81af6833c987ce7ce62654b3f58c1b82
Alli Aga <67258735+AlliAga@users.noreply.github.com>
2024-12-14 22:11:01 +0530

    Feature/auth (#3)
    * Home Feature: Bottom navigation bar Implemented, Lifestylle Screen Impemented, Navigation Option Model & Widget implemented
    
    * Home Feature: Bottom navigation bar Implemented, Lifestylle Screen Impemented, Navigation Option Model & Widget implemented
    
    * Home Feature: Bottom navigation bar Implemented, Lifestylle Screen Impemented, Navigation Option Model & Widget implemented
    
    * Home Feature: Daily Calory Card Implemented
    
    * Auth Feature: Gender field Implemented & Weight Indicator Implemented, Conditions Implemented

--- lib/features/auth/screens/step_one_screen.dart
+++ lib/features/auth/screens/step_one_screen.dart
@@ -1,39 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:optimally_me_app/features/auth/providers/step_one_provider.dart';
 import 'package:optimally_me_app/features/auth/widgets/step_header_widget.dart';
 import 'package:optimally_me_app/widgets/input_form_field_widget.dart';
+import 'package:optimally_me_app/widgets/multi_select_button_widget.dart';
+import 'package:optimally_me_app/widgets/select_indicator_widget.dart';
+import 'package:provider/provider.dart';
 
-class StepOneScreen extends StatelessWidget {
+class StepOneScreen extends StatefulWidget {
   const StepOneScreen({super.key});
+
+  @override
+  State<StepOneScreen> createState() => _StepOneScreenState();
+}
+
+class _StepOneScreenState extends State<StepOneScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
-        StepHeader(
+      padding: const EdgeInsets.only(bottom: 20),
+      children: [
+        const StepHeader(
           title: "Tell us about yourself?",
           description:
               "We will use your information to calculate your nutritional needs",
         ),
-        InputFormField(
+        const InputFormField(
           label: "First name",
           hintText: "Enter First Name",
         ),
-        InputFormField(
+        const InputFormField(
           label: "Last name",
           hintText: "Enter First Name",
         ),
-        InputFormField(
+        const InputFormField(
           label: "Date of birth",
           hintText: "DD / MM / YYYY",
         ),
-        InputFormField(
-          label: "Your Height (cm)",
-          hintText: "Enter your height",
+        MultiSelectButton(
+          options: [
+            MultiSelectButtonOption(
+              icon: Icons.male,
+              text: "Male",
+              value: "male",
+            ),
+            MultiSelectButtonOption(
+              icon: Icons.female,
+              text: "Female",
+              value: "female",
+            ),
+          ],
+          defaultSelected: "male",
         ),
-        InputFormField(
-          label: "Your weight (kg)",
-          hintText: "Enter your weight",
+        SelectIndicator(
+          options: const ["kg,cm", "lb,ft"],
+          onTap: context.read<StepOneProvider>().onWeightOptingIndexChange,
+          controller: _tabController,
         ),
+        Consumer<StepOneProvider>(
+          builder: (context, provider, child) {
+            return Column(
+              children: provider.weightOptionIndex == 0
+                  ? [
+                      const InputFormField(
+                        label: "Your Height (cm)",
+                        hintText: "Enter your height",
+                      ),
+                      const InputFormField(
+                        label: "Your weight (kg)",
+                        hintText: "Enter your weight",
+                      ),
+                    ]
+                  : [
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: InputFormField(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                label: "Your Height (ft)",
+                                hintText: "Enter your height",
+                              ),
+                            ),
+                            Expanded(
+                              child: InputFormField(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                label: "Your weight (lb)",
+                                hintText: "Enter your weight",
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+            );
+          },
+        )
       ],
     );
   }
---
+++ lib/widgets/select_indicator_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SelectIndicator extends StatelessWidget {
+  final EdgeInsetsGeometry? margin;
+  final TabController? controller;
+  final List<String> options;
+  final void Function(int index)? onTap;
+
+  const SelectIndicator({
+    super.key,
+    this.margin,
+    this.controller,
+    required this.options,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+      decoration: BoxDecoration(
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: TabBar(
+        dividerHeight: 0,
+        onTap: onTap,
+        padding: const EdgeInsets.all(2),
+        labelStyle: const TextStyle(color: Colors.white),
+        isScrollable: false,
+        labelPadding: const EdgeInsets.symmetric(vertical: 5),
+        indicator: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.black,
+          shape: BoxShape.rectangle,
+        ),
+        controller: controller,
+        indicatorSize: TabBarIndicatorSize.label,
+        tabs: options
+            .map(
+              (e) => Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(e),
+                ],
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
